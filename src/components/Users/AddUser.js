91523c92e4e6515c9a7a9e20857a3b880cb7a91e
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-11 11:42:01 +0300

    add card and styles

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,17 +1,22 @@
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
+
 const AddUser = () => {
   const handleAddUser = (event) => {
     event.preventDefault();
   };
   return (
-    <form>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age</label>
-      <input id="age" type="text" />
-      <button type="submit" onClick={handleAddUser}>
-        Add User
-      </button>
-    </form>
+    <Card cssClass={classes.input}>
+      <form>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age</label>
+        <input id="age" type="text" />
+        <button type="submit" onClick={handleAddUser}>
+          Add User
+        </button>
+      </form>
+    </Card>
   );
 };
 
